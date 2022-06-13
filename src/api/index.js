fd7c7d7205ffdd839897122204a376c0f2935255
RICKCOYL <rickoburu@gmail.com>
2022-06-13 18:19:57 +0300

    initial

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const url = 'https://mem97.herokuapp.com';
+const url = 'http://localhost:5000';
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
+export const fetchPosts = () => axios.get(`${url}/posts`);
+export const createPost = (newPost) => axios.post(`${url}/posts`, newPost);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
